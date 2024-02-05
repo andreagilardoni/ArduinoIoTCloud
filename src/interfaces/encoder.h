fd233112c613fb688f3489fff5470595bcac6a0c
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-05 09:12:47 +0100

    defining encoder/decoder interfaces

---
+++ src/interfaces/encoder.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <models/models.h>
+#include <stdint.h>
+
+using encoderFactory=std::function<Encoder*()>;
+
+class Encoder {
+public:
+    enum Status: uint8_t {
+        SUCCESS,
+        IN_PROGRESS,
+        ERROR
+    };
+
+    /**
+     * Encode a message into a buffer in a single shot
+     * @param msg: the message that needs to be encoded
+     * @param buf: the buffer the message will be encoded into
+     * @param len: the length of the provided buffer, value will be updated with the consumed len of the buffer
+     * @return SUCCESS: if the message is encoded correctly
+     *         ERROR:   error during the encoding of the message
+     */
+    virtual Status encode(Message* msg, uint8_t* buf, size_t& len) = 0;
+
+    /**
+     * sets the message that needs to be encoded, before iterativley encoding it
+     * @param Message the message that needs to be encoded
+     */
+    virtual void setMessage(Message* msg) = 0;
+
+    /**
+     * after having the Message struct set call this function to encode the message into a buffer
+     * this action is performed incrementally into chunks of buffers
+     * @param buf: the buffer the message will be encoded into
+     * @param len: the length of the provided buffer, value will be updated with the consumed len of the buffer
+     * @return SUCCESS:     the message is completely encoded
+     *         IN_PROGRESS: the message is encoded correctly so far, provide another buffer to continue
+     *         ERROR:       error during the encoding of the message
+     */
+    virtual Status encode(uint8_t* buf, size_t& len) = 0;
+};
