10f57a49752ef8c04339e2cf822e030356e8fcdf
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-21 17:29:43 +0100

    added OTA interface containing common features of OTA enabled devices

---
+++ src/utility/ota/OTAInterface.cpp
@@ -0,0 +1,232 @@
+/*
+   This file is part of ArduinoIoTCloud.
+
+   Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+/******************************************************************************
+ * INCLUDE
+ ******************************************************************************/
+
+// #include <AIoTC_Config.h>
+
+#if OTA_ENABLED
+
+#include "OTA.h"
+// #include <Arduino_DebugUtils.h>
+
+/******************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ ******************************************************************************/
+
+OTACloudProcessInterface::STATE_NAMES = {
+  "Resume",
+  "Idle",
+  "OtaAvailable",
+  "StartOTA",
+  "Fetch",
+  "VerifyOTA",
+  "FlashOTA",
+  "Reboot",
+  "DownloadFail",
+  "CRCValidationFail",
+  "Fail"
+};
+
+OTACloudProcessInterface::OTACloudProcessInterface(Client* client)
+: context(nullptr), state(Resume), previous_state(Resume), client(client) {
+
+}
+
+void OTACloudProcessInterface::handleMessage(Message* msg) {
+
+  // we can put conditions that states must follow here, or go into failed state
+  // we can receive an abort OTA message here and return to idle
+  // if(msg != nullptr && state == Fetch) { // We cannot receive Messages while in Fetch state
+  // }
+
+  switch(state) {
+  case Resume:        updateState(resume(msg));    break;
+  case Idle:          updateState(idle(msg));      break;
+  case OtaAvailable:  updateState(otaAvailable()); break;
+  case StartOTA:      updateState(startOTA());     break;
+  case Fetch:         updateState(fetch());        break;
+  case FlashOTA:      updateState(flashOTA());     break;
+  case Reboot:        updateState(reboot());       break;
+  case DownloadFail:
+  case CRCValidationFail:
+  case Fail:
+  default:            updateState(fail());
+  }
+}
+
+State OTACloudProcessInterface::resume(Message* msg) {
+  // we just booted the MCU, verify if we were performing an ota or just normally booted
+
+  if(!ota_in_progress) { // TODO replace with proper condition
+    return Idle;
+  }
+  // TODO verify that the ota was performed successfully
+
+  if(!OTA_SUCCESS) {
+    // TODO report some info about the failed ota
+
+    return Fail;
+  }
+
+  // TODO report that the ota performed was successful
+  return Idle
+}
+
+State OTACloudProcessInterface::idle(Message* msg) {
+  // if a msg arrived, it may be an OTAavailable, then go to otaAvailable
+  // otherwise do nothing
+  if(msg!=nullptr && msg.commandId == OTAavailable) {
+    // save info coming from this message
+    assert(context == nullptr); // This should never fail
+    context = (ota_context_t) malloc(sizeof(ota_context_t)); // TODO deallocate the struct, and decide where to do that
+
+    context->id            = (const char*)     malloc(strlen(msg.id) + 1);
+    context->url           = (const char*)     malloc(strlen(msg.url) + 1);
+    context->initialSha256 = (const uint8_t*)  malloc(32); // 32 is the fixed size for a sha256
+    context->finalSha256   = (const uint8_t*)  malloc(32);
+
+    strcpy(context->id, msg.id);
+    strcpy(context->url, msg.url);
+    memcpy(context->initialSha256, msg.initialSha256, 32);
+    memcpy(context->finalSha256, msg.finalSha256, 32);
+
+    context->calculatedCrc32     = 0xFFFFFFFF
+    context->state               = OtaDownloadHeader
+    context->headerCopiedBytes   = 0;
+
+    return OtaAvailable;
+  }
+
+  return Idle;
+}
+
+State OTACloudProcessInterface::otaAvailable() {
+  // depending on the policy decided on this device the ota process can start immediately
+  // or wait for confirmation from the user
+  if(policies & (ApprovalRequired | Approved) == ApprovalRequired ) {
+    return OtaAvailable;
+  } else {
+    policies &= ~Confirmation;
+    return StartOTA;
+  } // TODO add an abortOTA command? in this case delete the context
+}
+
+State OTACloudProcessInterface::startOTA() {
+  // Report that we are starting ota
+  statusReport();
+
+  assert(client != nullptr, "ERROR client wasn't set in OTACloudProcess");
+
+  // parse the url
+  // make the http get request
+  client->connect(context->url);
+
+  return Fetch;
+}
+
+State OTACloudProcessInterface::fetch() {
+  size_t buf_len = 100; // TODO should this be constexpr?
+  static uint8_t  buffer[buf_len];
+  int res = client->read(buffer, len);
+
+  assert(context != nullptr); // This should never fail
+
+  // TODO move this to a dedicated funtion, so that we chan use it as a callback instead of using memcpy
+  for(char* cursor=(char*)buffer; cursor<buffer+buf_len; ) {
+    switch(context->downloadState) {
+    case OtaDownloadHeader:
+
+      uint32_t copied = size < sizeof(context->header.buf) ? size : sizeof(context->header.buf);
+      memcpy(context->header.buf, buffer, copied);
+      cursor += copied;
+      context->headerCopiedBytes += copied;
+
+      // when finished go to next state
+      if(sizeof(context->header.buf) == context->headerCopiedBytes) {
+        context->downloadState = OtaDownloadFile;
+
+        context->calculatedCrc32 = crc_update(
+          context->calculatedCrc32,
+          &(context->header.header.magic_number),
+          sizeof(context->header) - offsetof(OTAHeader, header.magic_number)
+        );
+      }
+
+      break;
+    case OtaDownloadFile:
+      // TODO decompress on the fly
+
+      context->calculatedCrc32 = crc_update(
+          context->calculatedCrc32,
+          cursor,
+          buf_len - (cursor-buffer)
+        );
+      break;
+    default:
+      goto exit;
+    }
+  }
+
+exit:
+  // TODO verify that the information present in the ota header match the info in context
+  // TODO verify the magic number
+  if(context->downloadState == OtaDownloadCompleted) {
+    // Verify that the downloaded file size is matching the expected size ??
+    // this could distinguish between consistency of the downloaded bytes and filesize
+
+    // validate CRC
+    context->calculatedCrc32 ^= 0xFFFFFFFF;
+    if(context->header.crc32 == context->calculatedCrc32) {
+      client->stop(); // close the client, but do not deallocate it
+      return FlashOTA;
+    } else {
+      return CRCValidationFail;
+    }
+  } else if(context->downloadState == OtaDownloadError) {
+    return DownloadFail;
+  }
+}
+
+State OTACloudProcessInterface::fail() {
+  reportStatus();
+  reset();
+
+  // TODO clean the context pointer
+}
+
+void OTACloudProcessInterface::reportStatus() {
+  if(context == nullptr) {
+    // FIXME handle this case: ota not in progress
+    return;
+  }
+
+  union OTAStatusReport msg = {
+    OTAStatusReportId,
+    context->id,
+    "unknown", // FIXME put the proper expected value
+    millis(),  // FIXME put the proper expected value
+    context->report_couter++
+  };
+
+  deliver((Message*)&msg);
+  // TODO msg object do not extist after this call make sure it gets copied somewhere
+}
+
+#endif /* OTA_ENABLED */
---
+++ src/utility/ota/OTAInterface.h
@@ -0,0 +1,175 @@
+/*
+   This file is part of ArduinoIoTCloud.
+
+   Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+#pragma once
+
+/******************************************************************************
+ * INCLUDE
+ ******************************************************************************/
+
+#include <AIoTC_Config.h>
+
+#if OTA_ENABLED
+#include <Arduino.h>
+#include <Arduino_ConnectionHandler.h>
+#include <interfaces/CloudProcess.h>
+
+union HeaderVersion {
+  struct __attribute__((packed)) {
+    uint32_t header_version    :  6;
+    uint32_t compression       :  1;
+    uint32_t signature         :  1;
+    uint32_t spare             :  4;
+    uint32_t payload_target    :  4;
+    uint32_t payload_major     :  8;
+    uint32_t payload_minor     :  8;
+    uint32_t payload_patch     :  8;
+    uint32_t payload_build_num : 24;
+  } field;
+  uint8_t buf[sizeof(field)];
+  static_assert(sizeof(buf) == 8, "Error: sizeof(HEADER.VERSION) != 8");
+};
+
+union OTAHeader {
+  struct __attribute__((packed)) {
+    uint32_t len;
+    uint32_t crc32;
+    uint32_t magic_number;
+    HeaderVersion hdr_version;
+  } header;
+  uint8_t buf[sizeof(header)];
+  static_assert(sizeof(buf) == 20, "Error: sizeof(HEADER) != 20");
+};
+
+/******************************************************************************
+ * CLASS DECLARATION
+ ******************************************************************************/
+
+class OTACloudProcessInterface: public CloudProcess {
+public:
+  OTACloudProcessInterface();
+  OTACloudProcessInterface(const OTACloudProcessInterface&) = delete;
+  OTACloudProcessInterface(OTACloudProcessInterface&&) = delete;
+
+  enum State {
+    Resume,
+    Idle,
+    OtaAvailable,
+    StartOTA,
+    Fetch,
+    VerifyOTA,
+    FlashOTA,
+    Reboot,
+    DownloadFail,
+    CRCValidationFail,
+    Fail
+  };
+
+  static constexpr char* const STATE_NAMES[];
+
+  enum OtaFlags: uint16_t {
+    None              = 0,
+    ApprovalRequired  = 1,
+    Approved          = 1<<1
+  };
+
+  virtual void handleMessage(Message*);
+  // virtual CloudProcess::State getState();
+  // virtual void hook(State s, void* action);
+  virtual inline void update() { handleMessage(nullptr); }
+
+  inline void approveOta() { policies |= Confirmation; }
+
+  inline virtual void setClient(Client* c) { client = c; }
+protected:
+  // The following methods represent the FSM actions performed in each state
+
+  // the first state is 'resume', where we try to understand if we are booting after a ota
+  // the objective is to understand the result and report it to the cloud
+  virtual State resume(Message* msg=nullptr) = 0;
+
+  // this state is the normal state where no action has to be performed. We may poll the cloud
+  // for updates in this state
+  virtual State idle(Message* msg=nullptr);
+
+  // we go in this state if there is an ota available, depending on the policies implemented we may
+  // start the ota or wait for an user interaction
+  virtual State otaAvailable();
+
+  // we start the process of ota update and wait for the server to respond with the ota url and other info
+  virtual State startOTA();
+
+  // we start the download and decompress process
+  virtual State fetch() = 0;
+
+  // whene the download is correctly finished we set the mcu to use the newly downloaded binary
+  virtual State flashOTA() = 0;
+
+  // we reboot the device
+  virtual State reboot() = 0;
+
+  // if any of the steps described fail we get into this state and report to the cloud what happened
+  // then we go back to idle state
+  virtual State fail();
+
+  // TODO virtual =0 method used to reset the status of the failed ota operation
+  // TODO depending on the state in which we failed we may have different fail context
+  virtual void reset() = 0;
+
+  // this method will be called by fetch to write the ota binary on the device flash
+  // this method is directly called by fetch.
+  // This method will be overridden by the actual board implementation
+  virtual error_type writeFlash(uint8_t* const buffer, size_t len) = 0;
+
+  OtaPolicies policies; // TODO getter and setters for this
+
+  inline void updateState(State s) { if(state!=s) { sprevious_state = state; state = s; } }
+
+private:
+  void reportStatus();
+
+  State state, previous_state;
+  Client* client;
+
+  enum OTADownloadState: uint8_t {
+    OtaDownloadHeader,
+    OtaDownloadFile,
+    OtaDownloadCompleted,
+    OtaDownloadError
+  };
+
+  typedef struct {
+    // parameters present in the ota available message that are of interest of the process
+    const char*       id;
+    const char*       url; // TODO replace with parsed url struct
+    const uint8_t*    initialSha256;
+    const uint8_t*    finalSha256;
+
+    OTAHeader         header;
+    OTADownloadState  downloadState;
+    uint32_t          calculatedCrc32;
+    uint32_t          headerCopiedBytes;
+
+    // LZSS decoder
+  } ota_context_t;
+
+  ota_context_t *context;
+};
+
+
+
+#endif /* OTA_ENABLED */
