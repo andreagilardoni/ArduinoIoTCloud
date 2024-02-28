87f7eecde6fb6b8d4befe302d7473f08c0ea5edc
pennam <m.pennasilico@arduino.cc>
2024-02-28 16:13:16 +0100

    ArduinoIoTCloudTCP: remove unused function declaration

--- src/ArduinoIoTCloudTCP.h
+++ src/ArduinoIoTCloudTCP.h
@@ -109,8 +109,6 @@ class ArduinoIoTCloudTCP: public ArduinoIoTCloudClass
       _get_ota_confirmation = cb;
       _ask_user_before_executing_ota = true;
     }
-
-    void handle_OTARequest();
 #endif
 
   private:
