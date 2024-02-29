e3818273423840ff33c30101c989ed1fc33b6c46
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-29 13:35:02 +0100

    implemented rp2040 ota class

--- src/utility/ota/OTANanoRP2040.h
+++ src/utility/ota/OTANanoRP2040.h
@@ -1,18 +1,41 @@
 #pragma once
 
+#include "OTAInterface.h"
+
+#include "FATFileSystem.h"
+#include "FlashIAPBlockDevice.h"
+
 class NANO_RP2040OTACloudProcess: public OTACloudProcessInterface {
 public:
-  STM32H7OTACloudProcess();
-protected:
-  // we start the download and decompress process
-  virtual State fetch(Message* msg=nullptr);
+  NANO_RP2040OTACloudProcess(MessageStream *ms, ConnectionHandler* connection_handler=nullptr);
+  ~NANO_RP2040OTACloudProcess();
 
-  // when the download is completed we verify for integrity and correctness of the downloaded binary
-  // virtual State verifyOTA(Message* msg=nullptr); // TODO this may be performed inside download
+  virtual bool isOtaCapable() override;
+protected:
+  virtual OTACloudProcessInterface::State resume(Message* msg=nullptr) override;
+
+  virtual OTACloudProcessInterface::State startOTA() override;
 
   // whene the download is correctly finished we set the mcu to use the newly downloaded binary
-  virtual State flashOTA(Message* msg=nullptr);
+  virtual OTACloudProcessInterface::State flashOTA() override;
 
   // we reboot the device
-  virtual State reboot(Message* msg=nullptr);
+  virtual OTACloudProcessInterface::State reboot() override;
+
+  // write the decompressed char buffer of the incoming ota
+  virtual int writeFlash(uint8_t* const buffer, size_t len) override;
+
+  virtual void reset() override;
+
+  void* appStartAddress();
+  uint32_t appSize();
+  bool appFlashOpen() { return true; };
+  bool appFlashClose() { return true; };
+private:
+  FlashIAPBlockDevice flash;
+  FILE* decompressed;
+  mbed::FATFileSystem* fs;
+  static const char UPDATE_FILE_NAME[];
+
+  int close_fs();
 };
