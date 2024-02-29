07d38ca45dff6ff4baf0f76e18fa80fcc727930e
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-29 15:06:10 +0100

    avoid creating crc table for esp32 devices

--- src/utility/ota/OTAInterface.cpp
+++ src/utility/ota/OTAInterface.cpp
@@ -430,6 +430,7 @@ OTACloudProcessInterface::OtaContext::~OtaContext() {
   free(url);
 }
 
+#ifndef ARDUINO_ARCH_ESP32
 static const uint32_t crc_table[256] = {
     0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
     0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
@@ -477,6 +478,6 @@ uint32_t crc_update(uint32_t crc, const void * data, size_t data_len) {
 
   return crc & 0xffffffff;
 }
-
+#endif  // ARDUINO_ARCH_ESP32
 
 #endif /* OTA_ENABLED */
