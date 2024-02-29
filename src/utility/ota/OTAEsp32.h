763a30b0060986cd3a6a2ffb55ffd8af1f6b3738
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-29 14:02:00 +0100

    implemented esp32 ota class

--- src/utility/ota/OTAEsp32.h
+++ src/utility/ota/OTAEsp32.h
@@ -1,18 +1,48 @@
+/*
+  This file is part of the ArduinoIoTCloud library.
+
+  Copyright (c) 2024 Arduino SA
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #pragma once
+
+#include "OTAInterface.h"
 
 class ESP32OTACloudProcess: public OTACloudProcessInterface {
 public:
-  STM32H7OTACloudProcess();
+  ESP32OTACloudProcess(MessageStream *ms, ConnectionHandler* connection_handler=nullptr);
+
+  virtual bool isOtaCapable() override;
 protected:
-  // we start the download and decompress process
-  virtual State fetch(Message* msg=nullptr);
+  virtual OTACloudProcessInterface::State resume(Message* msg=nullptr) override;
+
+  // we are overriding the method of startOTA in order to download ota file on ESP32
+  virtual OTACloudProcessInterface::State startOTA() override;
 
   // when the download is completed we verify for integrity and correctness of the downloaded binary
   // virtual State verifyOTA(Message* msg=nullptr); // TODO this may be performed inside download
 
   // whene the download is correctly finished we set the mcu to use the newly downloaded binary
-  virtual State flashOTA(Message* msg=nullptr);
+  virtual State flashOTA();
 
   // we reboot the device
-  virtual State reboot(Message* msg=nullptr);
+  virtual State reboot();
+
+  // write the decompressed char buffer of the incoming ota
+  virtual int writeFlash(uint8_t* const buffer, size_t len) override;
+
+  virtual void reset() override;
+
+  void* appStartAddress();
+  uint32_t appSize();
+  bool appFlashOpen();
+  bool appFlashClose() { return true; };
+
+  void calculateSHA256(SHA256&) override;
+private:
+  const esp_partition_t *rom_partition;
 };
