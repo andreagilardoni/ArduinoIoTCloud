f6c48a763cce6a7a587b7ca05facccd50dba8e9a
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-29 14:02:00 +0100

    implemented esp32 ota class

--- src/utility/ota/OTAEsp32.cpp
+++ src/utility/ota/OTAEsp32.cpp
@@ -1,3 +1,65 @@
+#include "AIoTC_Config.h"
 #if defined(ARDUINO_ARCH_ESP32) && OTA_ENABLED
+#include "OTAEsp32.h"
+
+ESP32OTACloudProcess::ESP32OTACloudProcess(MessageStream *ms, ConnectionHandler* connection_handler)
+: OTACloudProcessInterface(ms, connection_handler) {
+
+}
+
+
+OTACloudProcessInterface::State ESP32OTACloudProcess::resume(Message* msg) {
+  return OtaBegin;
+}
+
+OTACloudProcessInterface::State ESP32OTACloudProcess::startOTA() {
+  Arduino_ESP32_OTA::Error ota_err = Arduino_ESP32_OTA::Error::None;
+
+  if ((ota_err = ota.begin()) != Arduino_ESP32_OTA::Error::None) {
+    DEBUG_ERROR("Arduino_ESP32_OTA::begin() failed with %d", static_cast<int>(ota_err));
+    return OtaDownloadFail; // FIXME
+  }
+
+  return Fetch;
+}
+
+OTACloudProcessInterface::State ESP32OTACloudProcess::fetch() {
+  Arduino_ESP32_OTA::Error ota_err = Arduino_ESP32_OTA::Error::None;
+
+  /* Download the OTA file from the web storage location. */
+  int const ota_download = ota.download(this->context->url);
+  if (ota_download <= 0) {
+    DEBUG_ERROR("Arduino_ESP_OTA::download() failed with %d", ota_download);
+    // return ota_download; // FIXME
+    return OtaDownloadFail;
+  }
+  DEBUG_VERBOSE("Arduino_ESP_OTA::download() %d bytes downloaded", static_cast<int>(ota_download));
+
+  return FlashOTA;
+}
+
+OTACloudProcessInterface::State ESP32OTACloudProcess::flashOTA() {
+  Arduino_ESP32_OTA::Error ota_err = Arduino_ESP32_OTA::Error::None;
+
+  if ((ota_err = ota.update()) != Arduino_ESP32_OTA::Error::None) {
+    DEBUG_ERROR("Arduino_ESP_OTA::update() failed with %d", static_cast<int>(ota_err));
+    return OtaDownloadFail; // FIXME
+  }
+
+  return Reboot;
+}
+
+OTACloudProcessInterface::State ESP32OTACloudProcess::reboot() {
+  ota.reset();
+
+  return Idle; // we won't reach this
+}
+
+void ESP32OTACloudProcess::reset() {
+}
+
+bool ESP32OTACloudProcess::isOtaCapable() {
+  return Arduino_ESP32_OTA::isCapable();
+}
 
 #endif // defined(ARDUINO_ARCH_ESP32) && OTA_ENABLED
--- src/utility/ota/OTAEsp32.h
+++ src/utility/ota/OTAEsp32.h
@@ -1,18 +1,36 @@
 #pragma once
 
+#include "OTAInterface.h"
+#include <Arduino_ESP32_OTA.h>
+
 class ESP32OTACloudProcess: public OTACloudProcessInterface {
 public:
-  STM32H7OTACloudProcess();
+  ESP32OTACloudProcess(MessageStream *ms, ConnectionHandler* connection_handler=nullptr);
+
+  virtual bool isOtaCapable() override;
 protected:
+  virtual OTACloudProcessInterface::State resume(Message* msg=nullptr) override;
+
+  // we are overriding the method of startOTA in order to download ota file on ESP32
+  virtual OTACloudProcessInterface::State startOTA() override;
+
   // we start the download and decompress process
-  virtual State fetch(Message* msg=nullptr);
+  virtual State fetch();
 
   // when the download is completed we verify for integrity and correctness of the downloaded binary
   // virtual State verifyOTA(Message* msg=nullptr); // TODO this may be performed inside download
 
   // whene the download is correctly finished we set the mcu to use the newly downloaded binary
-  virtual State flashOTA(Message* msg=nullptr);
+  virtual State flashOTA();
 
   // we reboot the device
-  virtual State reboot(Message* msg=nullptr);
+  virtual State reboot();
+
+  // write the decompressed char buffer of the incoming ota
+  virtual int writeFlash(uint8_t* const buffer, size_t len) override {};
+
+  virtual void reset() override;
+// private:
+public:
+  Arduino_ESP32_OTA ota;
 };
